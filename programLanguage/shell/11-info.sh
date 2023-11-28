7f55fdc9a1b5b879eeb31136ede34212af5511d3
D410c <331296441@qq.com>
2023-11-28 15:13:03 +0800

    自动提交 by linaro-alip 2023-11-28 15:13:02

--- programLanguage/shell/11-info.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-uptime | awk '{printf("\nCPU Load: %.2f\t", $(NF-2))}'
-free -m | awk 'NR==2{printf("Mem: %s/%sMB %.2f%%\n", $3,$2,$3*100/$2)}'
-cat /sys/class/thermal/thermal_zone0/temp|awk '{printf("CPU Temp: %.2f\t",$1/1000)}'
-df -h | awk '$NF=="/"{printf "Disk: %.1f/%.1fGB %s\n\n", $3,$2,$5}'
