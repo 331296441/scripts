0897847d19fdf105cac60ae095cb794c2d14bc27
D410c <331296441@qq.com>
2023-12-13 11:08:07 +0800

    自动提交 by linaro-alip 2023-12-13 11:08:07

---
+++ static-route.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# 定义telnet连接参数
+HOST="192.168.124.1"
+PORT="99"
+USERNAME="H3C"
+PASSWORD="12062603binbin"
+
+# 定义要执行的命令
+COMMAND="route add -net 11.11.11.0 netmask 255.255.255.0 gw 192.168.124.14"
+
+# 执行telnet连接并执行命令
+expect << EOF
+spawn telnet $HOST $PORT
+expect "Username:"
+send "$USERNAME\r"
+expect "Password:"
+send "$PASSWORD\r"
+expect "#"
+send "$COMMAND\r"
+expect "#"
+send "exit\r"
+EOF
